c55e394946df92e5347d51588059cd3dfe42dba6
CaoGaorong <caogaorong01@weidian.com>
2024-04-07 21:24:51 +0800

    writer异常情况对比

--- loader/src/main.rs
+++ loader/src/main.rs
@@ -8,14 +8,18 @@ use os_in_rust_common::vga:: {Writer, CharAttr, Color, ScreenBuffer};
 #[link_section = ".start"]
 pub extern "C" fn _start() {
     let mut writer = Writer::new(unsafe { &mut *(0xb8000 as *mut ScreenBuffer) }, CharAttr::new(Color::White, Color::Black, false));
+    // 正常输出
+    writer.write_byte(b'H');
+    writer.write_byte(b'e');
+    writer.write_byte(b'l');
+    writer.write_byte(b'l');
+    writer.write_byte(b'o');
 
-    for i in 0 .. 2000 {
-        writer.write_byte(b'a');
+    // 如果去掉lto=true，则无法输出
+    for ele in "Hello".bytes() {
+        writer.write_byte(ele);
     }
-    for i in  0 .. 10 {
-        writer.write_string("Hello, world");
-    }
-    
+
     loop {}
 }
 
